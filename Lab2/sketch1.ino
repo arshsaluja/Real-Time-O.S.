0a3522c23e51c3c48c0bdc0feaac03ea114238cc
Arsh Saluja <54842056+arshsaluja@users.noreply.github.com>
2020-08-20 22:43:16 +0530

    Create sketch1.ino

---
+++ Lab2/sketch1.ino
@@ -0,0 +1,18 @@
+
+void setup() {
+  //pinMode(LED_BUILTIN,OUTPUT);
+  
+  pinMode(13,OUTPUT);
+    
+}
+
+void loop() {
+    //digitalWrite(LED_BUILTIN,HIGH);
+    //delay(1000);
+    //digitalWrite(LED_BUILTIN,LOW);
+    //delay(1000);
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);
+    delay(1000);
+}
